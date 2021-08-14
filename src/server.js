c91e2d607a0210acc8cbf49edd756f1a34e5a67c
K_Doky <makingfunk@knou.ac.kr>
2021-08-14 11:49:01 +0900

    Room Count

--- src/server.js
+++ src/server.js
@@ -15,24 +15,45 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: { sids, rooms },
+        },
+    } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
     socket["nickname"] = "Anon";
     socket.onAny((event) => {
         console.log(`Socket Event:${event}`);
-    })
+    });
     socket.on("enter_room", (roomName, nickname, done) => {
         socket["nickname"] = nickname;
         socket.join(roomName);
         done();
         socket.to(roomName).emit("welcome", socket.nickname);
+        wsServer.sockets.emit("room_change", publicRooms());
     });
     socket.on("disconnecting", () => {
-        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
-    })
+        socket.rooms.forEach(room => 
+            socket.to(room).emit("bye", socket.nickname)
+        );
+    });
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
+    });
     socket.on("new_message", (msg, room, done) => {
         socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
-    })
+    });
 })
 
 httpServer.listen(3000, handleListen);
